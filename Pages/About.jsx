a6178d3ac07d1d1cb5631bff864eadb81e1d137f
RichieTheDev <111089062+RichieTheDev@users.noreply.github.com>
2022-10-12 08:22:50 +0100

    Add files via upload

---
+++ Pages/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1 className="text-black text-4xl ">About Us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime,
+        laboriosam unde. Perspiciatis, officia quasi. Eos modi earum commodi
+        sint? Inventore laboriosam ipsam id veritatis voluptatibus dolorem,
+        laudantium quam nam quaerat repellendus possimus tempora magni. Veniam
+        eveniet eius natus rem ex distinctio optio accusamus sapiente blanditiis
+        pariatur vero magni tenetur ratione, ea nam. Ullam, quis obcaecati, ipsa
+        reiciendis mollitia veritatis, molestias dignissimos nisi nulla
+        recusandae delectus laboriosam similique. Eos voluptatibus, sit aliquid
+        fugit, amet veniam veritatis praesentium illo quasi commodi quidem
+        eveniet deserunt similique odit maxime accusamus aut optio
+        exercitationem temporibus neque distinctio ullam pariatur libero. Error
+        recusandae sunt id numquam molestiae sed quibusdam labore dolor sequi
+        aspernatur fugiat dolorum reprehenderit minus, nihil asperiores enim
+        necessitatibus facere, doloremque possimus ad. Ad ratione ipsa
+        accusantium eligendi, nobis repellendus itaque cum autem, expedita minus
+        quibusdam eum asperiores magnam consequatur, praesentium vero? Aliquid
+        libero necessitatibus voluptatibus aspernatur vel modi fugit cumque
+        doloribus soluta. Animi nesciunt nam, tenetur fuga laboriosam, saepe
+        adipisci illum laudantium officiis, id accusantium. Eveniet itaque ipsum
+        eos, expedita labore officia neque tempore tempora quasi exercitationem
+        at impedit, nam quam quaerat molestiae dolores corporis rerum.
+        Distinctio, architecto, laborum deleniti dignissimos et iure accusamus
+        harum quae, doloremque doloribus est fugit nostrum amet adipisci?
+      </p>
+    </>
+  );
+};
+
+export default About;
