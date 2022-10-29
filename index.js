e14548e6fc2d3a33cd1dc89081c2c18c79c8777d
RichieTheDev <111089062+RichieTheDev@users.noreply.github.com>
2022-10-29 14:32:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import Faqs from "./Pages/Faqs";
+import Contact from "./Pages/Contact";
+import Navbar from "./components/Navbar";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Faqs" element={<Faqs />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
